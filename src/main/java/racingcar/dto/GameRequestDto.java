7480f84707cc954096454a382c78f4e30cc6f1c0
jinwon1234 <ljw0626@naver.com>
2025-10-22 23:45:45 +0900

    feat: GameRequestDto에 getCars 메서드 추가

--- src/main/java/racingcar/dto/GameRequestDto.java
+++ src/main/java/racingcar/dto/GameRequestDto.java
@@ -1,7 +1,21 @@
 package racingcar.dto;
 
+import racingcar.domain.Car;
+
+import java.util.Arrays;
+import java.util.List;
+
 public record GameRequestDto(
         String carNames,
         int gameCount
 ) {
+
+    public List<Car> getCars() {
+
+        return Arrays.stream(carNames.split(","))
+                .map(name -> {
+                    if (name.isBlank() || name.length() > 5) throw new IllegalArgumentException();
+                    return new Car(name);
+                }).toList();
+    }
 }
