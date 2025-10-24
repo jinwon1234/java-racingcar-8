a53fd6a12f691609b7a9232935c27daaecb70fa3
jinwon1234 <ljw0626@naver.com>
2025-10-24 23:44:36 +0900

    feat: Application main 메서드에 실행을 위한 클래스 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import racingcar.controller.RacingGameController;
+import racingcar.controller.RacingGameControllerV1;
+import racingcar.dto.GameRequestDto;
+import racingcar.dto.GameResponseDto;
+import racingcar.inputhandler.ConsoleInputHandler;
+import racingcar.inputhandler.InputHandler;
+import racingcar.outputhandler.ConsoleOutputHandler;
+import racingcar.outputhandler.OutputHandler;
+import racingcar.service.RacingGameService;
+import racingcar.service.RacingGameServiceV1;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputHandler inputHandler = new ConsoleInputHandler();
+        OutputHandler outputHandler = new ConsoleOutputHandler();
+        RacingGameService racingGameService = new RacingGameServiceV1();
+        RacingGameController racingGameController = new RacingGameControllerV1(racingGameService);
+
+        GameRequestDto gameRequestDto = inputHandler.getInput();
+        GameResponseDto gameResult = racingGameController.getGameResult(gameRequestDto);
+        outputHandler.response(gameResult);
     }
 }
