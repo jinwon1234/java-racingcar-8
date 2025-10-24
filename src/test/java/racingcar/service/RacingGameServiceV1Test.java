3476eef60e74101641fe5dbb92794a8177ef57c8
jinwon1234 <ljw0626@naver.com>
2025-10-24 23:23:35 +0900

    feat: RacingGameService 테스트 추가

---
+++ src/test/java/racingcar/service/RacingGameServiceV1Test.java
@@ -0,0 +1,32 @@
+package racingcar.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+import racingcar.dto.GameResponseDto;
+
+import java.util.List;
+
+import static org.assertj.core.api.SoftAssertions.*;
+
+class RacingGameServiceV1Test {
+
+    RacingGameServiceV1 racingGameServiceV1 = new RacingGameServiceV1();
+
+
+    @Test
+    @DisplayName("게임 성공적으로 실행")
+    void gameExecutionSuccess() {
+        // given
+        List<Car> cars = List.of(new Car("pobi"), new Car("woni"), new Car("jun"));
+
+        // when
+        GameResponseDto gameResponseDto = racingGameServiceV1.executionGame(cars, 5);
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(gameResponseDto.winnerNames()).containsAnyOf("pobi", "woni", "jun");
+            softly.assertThat(gameResponseDto.results()).hasSize(5);
+        });
+    }
+}
