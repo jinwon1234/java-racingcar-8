d5d6ac243e66fe371927b05bcf754f63a98d91bd
jinwon1234 <ljw0626@naver.com>
2025-10-26 11:22:56 +0900

    feat: ApplicationTest에 통합 테스트 작성

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,12 +1,16 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static racingcar.message.ErrorMessage.*;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -30,6 +34,36 @@ class ApplicationTest extends NsTest {
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"      ", ""})
+    @DisplayName("자동차 이름 입력값으로 빈값이 들어오면 실패")
+    void carNamesBlankFail(String carNames) {
+        assertThatThrownBy(()-> runException(carNames, "5"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_INPUT);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi,jinwon,jun", "pobi,,jun,,jinwo"})
+    @DisplayName("자동차 이름이 1자 이상 5자 이하가 아니면 실패")
+    void carNamesLengthFail(String carNames) {
+        assertThatThrownBy(()-> runException(carNames, "5"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_CAR_NAME);
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123안213", "12313녕", "하123세", "요"})
+    @DisplayName("숫자가 아닌 형태로 게임 횟수가 입력되면 실패")
+    void carNamesSizeFail(String gameCount) {
+        assertThatThrownBy(()-> runException("pobi,woni", gameCount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_GAME_COUNT);
+    }
+
+
 
     @Override
     public void runMain() {
